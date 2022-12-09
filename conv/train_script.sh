e5252a9d635e45380c5268d45c0e434673884eaa
Robert King <robertkng104@gmail.com>
2022-12-08 17:25:07 -0800

    write better log

--- conv/train_script.sh
+++ conv/train_script.sh
@@ -6,6 +6,7 @@
 #SBATCH --mem=32G
 #SBATCH --gpus=1
 #SBATCH --partition=serc
+
 export DATADIR=/scratch/users/robcking/cs229_data
 module load py-pytorch/1.11.0_py39 py-scikit-learn/1.0.2_py39
-python3 train.py $@
+python3 train.py $@ >> $2
